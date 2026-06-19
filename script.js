cbac86f8d310160b3cc96480025f17e199ac2f11
Polina Spirina <polly_sp@mail.ru>
2026-06-19 04:03:17 +0300

    good

---
+++ script.js
@@ -0,0 +1,100 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxbEpp34PVd54VOF5mpfnfNVVe01Ye5P9jfeoEEyu9t74LjbaoSAn9ttSPcWCuaZLe_/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const formSendResult = document.querySelector(".form-send");
+  formSendResult.textContent = "";
+  const drinks = formData.getAll("drinks");
+  const submitButton = document.querySelector(".button");
+  submitButton.textContent = "Отправка...";
+  // Преобразуем массив в строку с разделителем (например, запятая)
+  const drinksString = drinks.join(", ");
+
+  // Создаем новый FormData и добавляем все поля
+  const newFormData = new FormData();
+  newFormData.append("name", formData.get("name"));
+  newFormData.append("presence", formData.get("presence"));
+
+  newFormData.append("drinks", drinksString);
+
+  try {
+    const response = await fetch(scriptURL, {
+      method: "POST",
+      body: newFormData,
+    });
+    formSendResult.textContent = "Спасибо! Анкета отправлена.";
+    form.reset(); // Очищаем форму
+  } catch (error) {
+    formSendResult.textContent = "Повторите попытку позже.";
+    console.error(error);
+  } finally {
+    // Возвращаем кнопку в исходное состояние
+    submitButton.textContent = "Отправить";
+  }
+});
+
+const nameInput = document.getElementById("name");
+const errorElement = document.getElementById("error-text");
+
+nameInput.addEventListener("invalid", function (event) {
+  event.preventDefault();
+  if (this.validity.valueMissing) {
+    errorElement.classList.add("show");
+  }
+});
+
+nameInput.addEventListener("input", function () {
+  if (this.value.trim() !== "") {
+    errorElement.classList.remove("show");
+  }
+});
+
+document.querySelectorAll('input[name="presence"]').forEach((radio) => {
+  radio.addEventListener("invalid", function (e) {
+    e.preventDefault();
+    document.getElementById("presenceError").classList.add("show");
+    return false;
+  });
+});
+
+document.querySelectorAll('input[name="presence"]').forEach((radio) => {
+  radio.addEventListener("change", function () {
+    document.getElementById("presenceError").classList.remove("show");
+  });
+});
+
+
+
+function startCountdown(targetDate) {
+  function updateTimer() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    if (distance < 0) {
+      document.getElementById("timer").style.display = "none";
+      document.getElementById("datetime").textContent = "Мы стали семьей!";
+      return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+  }
+
+  updateTimer();
+  setInterval(updateTimer, 1000);
+}
+
+const newYear = new Date(2026, 7, 8, 9, 0, 0).getTime();
+startCountdown(newYear);
